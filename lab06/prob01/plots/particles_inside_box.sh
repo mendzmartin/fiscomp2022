19733fff038b247a29b5922bce68acb6ecbf6716
mendzmartin <chmartinmendez@gmail.com>
2022-06-26 23:10:07 -0300

    26/06/22-23:10h

--- lab06/prob01/plots/particles_inside_box.sh
+++ lab06/prob01/plots/particles_inside_box.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/bash
 index=0
-for i in {10..20}
+for i in {10..100}
     do
         gnuplot -e "set terminal jpeg;
-        set title 'brownian dynamics - steady state';
+        set title 'brownian dynamics - steady state - L=8.5499';
+        set view 65,35;
+        set xrange[-5:5];set yrange[-5:5];set zrange[-5:5];
+        set xyplane at -6; L=8.5499;
         set xlabel 'x coordinate' rotate parallel;
         set ylabel 'y coordinate' rotate parallel;
         set zlabel 'z coordinate' rotate parallel;
-        splot '../results/picture${i}.dat' u 1:2:3 w p pt 7 ps 0.2 lc 'red' notitle" > ../results/movie${index}.jpeg
+        splot '../results/picture${i}.dat' u 1:2:3 w p pt 7 ps 0.2 lc 'dark-red' notitle,\
+        '../results/fcc.dat' u 1:(-L/2):(-L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u 1:(L/2):(L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u 1:(-L/2):(L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u 1:(L/2):(-L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (-L/2):2:(-L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (L/2):2:(L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (-L/2):2:(L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (L/2):2:(-L/2) w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (-L/2):(-L/2):3 w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (L/2):(L/2):3 w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (-L/2):(L/2):3 w l lw 0.2 lc 'black' notitle,\
+    '../results/fcc.dat' u (L/2):(-L/2):3 w l lw 0.2 lc 'black' notitle" > ../results/movie${index}.jpeg
         index=$((index+1))
     done
 
 
 ffmpeg -r 9 -i ../results/movie%d.jpeg movie.mp4
 
-for i in {0..10}
+for i in {0..100}
     do 
     rm -f ../results/movie${i}.jpeg
     done
