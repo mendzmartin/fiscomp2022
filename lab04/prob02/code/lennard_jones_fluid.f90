b85e9191acce180cb1ba1cf4b11d9a4b95c34df8
mendzmartin <chmartinmendez@gmail.com>
2022-07-11 10:16:13 -0300

    11/07/2022-10:16h

--- lab04/prob02/code/lennard_jones_fluid.f90
+++ lab04/prob02/code/lennard_jones_fluid.f90
@@ -1,137 +1,113 @@
 ! make clean && make lennard_jones_fluid.o && ./lennard_jones_fluid.o
 program lennard_jones_fluid
-    use module_precision
+    use module_precision;use module_mc_lennard_jones
     implicit none
+    integer(sp), parameter   :: MC_step=10000_sp,MC_step_trans=3000_sp ! Monte Carlo step total and transitory
+    ! VARIABLES y PARAMETROS GENERALES
+    integer(sp), parameter   :: n_p=256_sp                             ! cantidad de partículasa
+    real(dp),    parameter   :: delta_time=0.001_dp                    ! paso temporal
+    integer(sp), parameter   :: time_eq=3000_sp,&                     ! pasos de equilibración
+                                time_run=10000_sp,&                    ! pasos de evolucion en el estado estacionario
+                                ensamble_step=10_sp                    ! pasos de evolución para promedio en ensamble
+    real(dp),    parameter   :: T_adim_ref=1._dp                       ! temperatura de referencia adimensional
+    real(dp),    parameter   :: density=0.8_dp                         ! densidad (particulas/volumen)
+    real(dp),    parameter   :: r_cutoff=2.5_dp,mass=1._dp             ! radio de corte de interacciones y masa    
+    real(dp),    parameter   :: dinamic_viscosity=2.87_dp
+    real(dp),    parameter   :: pi=4._dp*atan(1._dp)
+    real(dp),    parameter   :: diffusion_coeff=T_adim_ref*&
+                                 (1._dp/(3._dp*pi*dinamic_viscosity)) 
+    real(dp),    allocatable :: x_vector(:),y_vector(:),z_vector(:)    ! componentes de las posiciones/particula
+    real(dp),    allocatable :: x_vector_noPBC(:),y_vector_noPBC(:),&  ! componentes de la posición sin PBC
+                                z_vector_noPBC(:)
+    real(dp),    allocatable :: force_x(:),force_y(:),force_z(:)       ! componentes de la fuerza/particula
+    integer(sp)              :: i,j,istat,index                        ! loop index
+    real(dp)                 :: time_end,time_start                    ! tiempos de CPU
+    ! VARIABLES PARA COMPUTAR ENERGÍA POTENCIAL
+    real(dp)                 :: U_adim,time,press  ! observables
+    real(dp)                 :: U_med,var_U,err_U
+    real(dp)                 :: s1_U,s2_U
+    ! VARIABLES PARA COMPUTAR PRESIÓN OSMÓTICA
+    real(dp)                 :: press_med,var_press,err_press
+    real(dp)                 :: s1_press,s2_press
 
-    real(dp)                 :: density          ! densidad de partículas
-    integer(sp)              :: n_p              ! numero total de partículas (125,216,512)
-    integer(sp)              :: i,istat
-    real(dp),    allocatable :: sc_vector_x(:),&
-                                sc_vector_y(:),&
-                                sc_vector_z(:)
+    call cpu_time(time_start)
 
-    20 format(2(E12.4,x),E12.4);21 format(2(A12,x),A12)
+    allocate(x_vector(n_p),y_vector(n_p),z_vector(n_p))
+    x_vector(:)=0._dp;y_vector(:)=0._dp;z_vector(:)=0._dp
+    allocate(x_vector_noPBC(n_p),y_vector_noPBC(n_p),z_vector_noPBC(n_p))
+    x_vector_noPBC(:)=0._dp;y_vector_noPBC(:)=0._dp;z_vector_noPBC(:)=0._dp
+    allocate(force_x(n_p),force_y(n_p),force_z(n_p))
+    force_x(:)=0._dp;force_y(:)=0._dp;force_z(:)=0._dp
 
-    ! estructura cubica simple (SC)
-    n_p=125_sp;density=1._dp
-    allocate(sc_vector_x(n_p),sc_vector_y(n_p),sc_vector_z(n_p))
-    sc_vector_x=0._dp;sc_vector_y=0._dp;sc_vector_z=0._dp
-    call initial_particle_configuration_sc(n_p,density,sc_vector_x,sc_vector_y,sc_vector_z)
-    open(10,file='../results/lennard_jones_fluid_01.dat',status='replace',action='write',iostat=istat)
-    write(*,*) 'istat(10file) = ',istat
-    write(10,21) 'rx_sc','ry_sc','rz_sc'
-    do i=1,n_p;write(10,20) sc_vector_x(i),sc_vector_y(i),sc_vector_z(i);end do
-    close(10)
-    deallocate(sc_vector_x,sc_vector_y,sc_vector_z)
+    ! generamos configuración inicial (FCC structure)
+    call initial_lattice_configuration(n_p,density,x_vector,y_vector,z_vector,2)
+    x_vector_noPBC(:)=x_vector(:);y_vector_noPBC(:)=y_vector(:);z_vector_noPBC(:)=z_vector(:)
+    ! computamos fuerzas en el tiempo inicial
+    call f_lj_total(x_vector,y_vector,z_vector,r_cutoff,n_p,density,force_x,force_y,force_z)
 
-    ! estructura cubica centrada en el cuerpo (FCC)
-    n_p=256_sp;density=0.8_dp
-    allocate(sc_vector_x(n_p),sc_vector_y(n_p),sc_vector_z(n_p))
-    sc_vector_x=0._dp;sc_vector_y=0._dp;sc_vector_z=0._dp
-    call initial_particle_configuration_fcc(n_p,density,sc_vector_x,sc_vector_y,sc_vector_z)
-    open(10,file='../results/lennard_jones_fluid_02.dat',status='replace',action='write',iostat=istat)
-    write(*,*) 'istat(10file) = ',istat
-    write(10,21) 'rx_fcc','ry_fcc','rz_fcc'
-    do i=1,n_p;write(10,20) sc_vector_x(i),sc_vector_y(i),sc_vector_z(i);end do
-    close(10)
-    deallocate(sc_vector_x,sc_vector_y,sc_vector_z)
+    ! TRANSITORIO
+    do i=1,time_eq
+        call evolution_monte_carlo(n_p,x_vector,y_vector,z_vector,&
+        x_vector_noPBC,y_vector_noPBC,z_vector_noPBC,&
+        U_adim,T_adim,r_cutoff,density)
+        call evolution_bd(n_p,x_vector,y_vector,z_vector,&
+            x_vector_noPBC,y_vector_noPBC,z_vector_noPBC,&
+            delta_time,mass,r_cutoff,density,force_x,force_y,force_z,&
+            dinamic_viscosity,diffusion_coeff)
+    end do
 
+    
 end program lennard_jones_fluid
 
-subroutine initial_particle_configuration_sc(n_p,density,x_coord_vector,y_coord_vector,z_coord_vector)
-    use module_precision
-    implicit none
-    integer(sp), intent(in)    :: n_p                   ! numero total de partículas
-    real(dp),    intent(in)    :: density               ! densidad de partículas
-    real(dp),    intent(inout) :: x_coord_vector(n_p),& ! coordenadas de los vectores posición
-                                  y_coord_vector(n_p),&
-                                  z_coord_vector(n_p)
+subroutine lennard_jones_relaxation(n,MC_step,MC_step_trans,m,file_num,aux_matrix_pbc,T_start,T_end,Tc_adim,U_adim,Madim)
+    use module_precision;use module_mc_lennard_jones
+    implicit none 
+    real(dp),    intent(in)     :: T_end,T_start,Tc_adim
+    integer(sp), intent(in)     :: n,m,file_num,MC_step,MC_step_trans
+    integer(sp), intent(inout)  :: aux_matrix_pbc(n+2,n+2)
+    real(dp),    intent(inout)  :: U_adim,Madim
+    integer(sp)                 :: i,j
+    real(dp)                    :: T_step,T_adim
+    real(dp)                    :: U_med_adim,sigma_U,error_U
+    real(dp)                    :: M_med_adim,sigma_M,error_M
+    real(dp)                    :: s0,s1_U,s2_U,s1_M,s2_M ! variables para hacer estadística
 
-    integer(sp)           :: i,j,k,index
-    integer(sp)           :: m                  ! numero total de coordenadas (x,y,z)
-    real(dp)              :: L                  ! longitud macroscópica
-    real(dp)              :: a                  ! parámetro de red
-    real(dp), allocatable :: aux_vector_x(:),&
-                             aux_vector_y(:),&
-                             aux_vector_z(:)
-
-    L=(real(n_p,dp)*(1._dp/density))**(1._dp/3._dp)
-
-    m=int(anint(n_p**(1._dp/3._dp),dp),sp)
-    a=L*(1._dp/real(m-1,dp))
-    allocate(aux_vector_x(m),aux_vector_y(m),aux_vector_z(m))
-    aux_vector_x=0._dp;aux_vector_y=0._dp;aux_vector_z=0._dp
-    ! creamos una configuración SC
-    ! llenamos los vectores por coordenadas (todos diferentes)
-    do i=1,m
-        aux_vector_x(i)=real(i-1,dp)*a
-        aux_vector_y(i)=real(i-1,dp)*a
-        aux_vector_z(i)=real(i-1,dp)*a
+    20 format(I12,x,E12.4,x,E12.4)
+    T_step=abs(T_end-T_start)*(1._dp/real(m-1_sp,dp))
+    ! termalizo hasta una temperatura anterior a la buscada
+    do j=1,m-1
+        T_adim=T_start+T_step*real(j-1_sp,dp)
+        write(*,'(A12,E12.4)') 'T_adim=',T_adim
+        do i=1,MC_step
+            call MC_step_relaxation(1_sp,n,aux_matrix_pbc,T_adim,U_adim)
+        end do
     end do
-    ! llenamos las coordenadas de cada particula
-    index=0
-    do i=1,m;do j=1,m;do k=1,m
-        index=index+1
-        x_coord_vector(index)=aux_vector_x(i)
-        y_coord_vector(index)=aux_vector_y(j)
-        z_coord_vector(index)=aux_vector_z(k)
-    end do;end do;end do
-    deallocate(aux_vector_x,aux_vector_y,aux_vector_z)
-end subroutine initial_particle_configuration_sc
+    ! termalizo a la temperatura buscada
+    write(*,'(A12,E12.4)') 'T_adim=',T_end
+    s0=0._dp;s1_U=0._dp;s2_U=0._dp;s1_M=0._dp;s2_M=0._dp
+    do i=1,MC_step
+        call MC_step_relaxation(1_sp,n,aux_matrix_pbc,T_end,U_adim)
+        Madim=M_adim(aux_matrix_pbc,n) ! Magnetización
+        ! datos para hacer estadística en steady state
+        if (i>=MC_step_trans) then
+            s0=s0+1._dp
+            s2_U=s2_U+U_adim*U_adim;s1_U=s1_U+U_adim ! energía
+            s2_M=s2_M+Madim*Madim;s1_M=s1_M+Madim    ! magnetización
+        end if
+        write(file_num,20) i,U_adim,Madim
+    end do
 
-! Subroutine to set up fcc lattice
-subroutine initial_particle_configuration_fcc(n_p,density,x_coord_vector,y_coord_vector,z_coord_vector)
-    use module_precision
-    implicit none
-    integer(sp), intent(in)    :: n_p                   ! numero total de partículas
-    real(dp),    intent(in)    :: density               ! densidad de partículas
-    real(dp),    intent(inout) :: x_coord_vector(n_p),& ! coordenadas de los vectores posición
-                                  y_coord_vector(n_p),&
-                                  z_coord_vector(n_p)
+    !calculamos media,desviacion estándar, varianza y error
+    U_med_adim=s1_U*(1._dp/s0)
+    sigma_U=sqrt((s2_U-s0*U_med_adim*U_med_adim)*(1._dp/(s0-1._dp)))
+    error_U=sigma_U*(1._dp/sqrt(s0-1._dp))
+    M_med_adim=s1_M*(1._dp/s0)
+    sigma_M=sqrt((s2_M-s0*M_med_adim*M_med_adim)*(1._dp/(s0-1._dp)))
+    error_M=sigma_M*(1._dp/sqrt(s0-1._dp))
 
-    integer(sp)           :: i,j,k,index2,index
-    real(dp)              :: L                  ! logitud macroscópica por dimensión
-    real(dp)              :: a                  ! parámetro de red
-    real(dp)              :: n_unitcells ! numero de celdas unidad por dimension
-    real(dp), allocatable :: aux_matrix(:,:)
+    write(*,'(A9,I2,A2,I2)')  'Lattice=',n,'x',n
+    write(*,'(A14,E12.4)')    'M_exact_adim=',M_exact_adim(n,T_end,Tc_adim)
+    write(*,'(2(A14,E12.4))') 'M_med_adim=',M_med_adim,'error_M',error_M
+    write(*,'(2(A14,E12.4))') 'U_med_adim=',U_med_adim,'error_U',error_U
 
-    allocate(aux_matrix(4,3));aux_matrix(4,3)=0._dp
-    
-    L=(n_p*(1._dp/density))**(1._dp/3._dp)   
-    n_unitcells=anint((n_p*0.25_dp)**(1._dp/3._dp),dp) 
-    a=L*(1._dp/n_unitcells)
-    !a=(4*(1._dp/density))**(1._dp/3._dp)
-
-    aux_matrix(:,:)=a*0.5_dp
-    aux_matrix(1,:)=0.0_dp;aux_matrix(2,3)=0.0_dp
-    aux_matrix(3,2)=0.0_dp;aux_matrix(4,1)=0.0_dp
-    
-    ! Going through all boxes in the system
-    index=0
-    do i=1,int(n_unitcells,sp);do j=1,int(n_unitcells,sp);do k=1,int(n_unitcells,sp);do index2=1,4
-        index=index+1
-        x_coord_vector(index) = aux_matrix(index2,1) + real(i-1,dp)*a
-        y_coord_vector(index) = aux_matrix(index2,2) + real(j-1,dp)*a
-        z_coord_vector(index) = aux_matrix(index2,3) + real(k-1,dp)*a
-    end do;end do;end do;end do
-
-    deallocate(aux_matrix)
-    
-end subroutine initial_particle_configuration_fcc
-
-function u_lj(x,y,z)
-    use module_precision
-    implicit none
-    real(dp), intent(in) :: x,y,z           ! coordenadas
-    real(dp)             :: factor1,factor2 ! factores posiciones
-    real(dp)             :: r               ! vector posición adimensional
-    real(dp)             :: u_lj            ! lennard_jones_potential
-    integer(sp)          :: i
-
-    r=sqrt(x*x+y*y+z*z)
-    factor1=1._dp;factor2=1._dp
-    do i=1,12;factor1=factor1*(1._dp/r);end do
-    do i=1,6;factor2=factor2*(1._dp/r);end do
-
-        u_lj=4*(factor1-factor2)
-end function u_lj
+end subroutine lennard_jones_relaxation
